1b7baeeb834c0115e5289a7cfb6b2f3d1a6f56f2
Nemoto <c0a2211718@edu.teu.ac.jp>
2023-11-28 14:53:16 +0900

    練習１～３：矢印キーでこうかとんを移動まで

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,8 +1,17 @@
 import sys
 import pygame as pg
-
+import random
 
 WIDTH, HEIGHT = 1400, 800
+
+delta = {
+    pg.K_UP: (0, -5),
+    pg.K_DOWN: (0, +5),
+    pg.K_LEFT: (-5, 0),
+    pg.K_RIGHT: (+5,0)
+
+
+}
 
 
 def main():
@@ -11,18 +20,42 @@ def main():
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+
+    kk_rct = kk_img.get_rect()
+    kk_rct.center = 900,400
+
+    bb_img = pg.Surface((20,20))
+    bb_img.set_colorkey((0,0,0))
+    pg.draw.circle(bb_img, (255,0,0), (10,10), 10)
+
+    bb_rct = bb_img.get_rect()
+    bb_rct.centerx = random.randint(0, WIDTH)
+    bb_rct.centery = random.randint(0, HEIGHT)
+    vx, vy = +5, +5
+
     clock = pg.time.Clock()
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
+            
+        key_lst = pg.key.get_pressed()
+        sum_mv = [0,0]
+        for k, tpl in delta.items():
+            if key_lst[k]:
+                sum_mv[0] += tpl[0]
+                sum_mv[1] += tpl[1]
 
         screen.blit(bg_img, [0, 0])
-        screen.blit(kk_img, [900, 400])
+        
+        kk_rct.move_ip(sum_mv[0], sum_mv[1])
+        screen.blit(kk_img, kk_rct)
+        bb_rct.move_ip(vx, vy)
+        screen.blit(bb_img, bb_rct)
         pg.display.update()
         tmr += 1
-        clock.tick(10)
+        clock.tick(50)
 
 
 if __name__ == "__main__":
