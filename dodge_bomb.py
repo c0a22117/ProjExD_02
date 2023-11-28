c17a64944fb1dcf8923e343bd64437c2cc2d82ff
Nemoto <c0a2211718@edu.teu.ac.jp>
2023-11-28 15:28:23 +0900

    練習４～５：こうかとんが爆弾と衝突したらまで

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -9,9 +9,15 @@ delta = {
     pg.K_DOWN: (0, +5),
     pg.K_LEFT: (-5, 0),
     pg.K_RIGHT: (+5,0)
-
-
 }
+
+def check_bound(rct: pg.Rect) -> tuple[bool, bool]:
+    yoko, tate = True, True
+    if rct.left < 0 or WIDTH < rct.right:  # 横方向はみ出し判定
+        yoko = False
+    if rct.top < 0 or HEIGHT < rct.bottom:  # 縦方向はみ出し判定
+        tate = False
+    return yoko, tate
 
 
 def main():
@@ -40,6 +46,10 @@ def main():
             if event.type == pg.QUIT: 
                 return
             
+        if kk_rct.colliderect(bb_rct):
+            print("Game Over")
+            return
+            
         key_lst = pg.key.get_pressed()
         sum_mv = [0,0]
         for k, tpl in delta.items():
@@ -50,7 +60,14 @@ def main():
         screen.blit(bg_img, [0, 0])
         
         kk_rct.move_ip(sum_mv[0], sum_mv[1])
+        if check_bound(kk_rct) != (True, True):
+            kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
         screen.blit(kk_img, kk_rct)
+        yoko, tate = check_bound(bb_rct)
+        if not yoko:
+            vx *= -1
+        if not tate:
+            vy *= -1
         bb_rct.move_ip(vx, vy)
         screen.blit(bb_img, bb_rct)
         pg.display.update()
